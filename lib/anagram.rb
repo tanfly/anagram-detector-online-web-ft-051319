cae05af197d17edcb69ce486122b6a1036d4e0c5
Stella McEwen <mcewen.stella@gmail.com>
2019-05-24 15:25:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,17 +6,7 @@ class Anagram
   end
   
   def match(maybe_anagrams_array)
- letters = word.split 
- maybe_anagram_one = letters.sort
- maybe_anagrams_array.find_all do |words|
-   letters_two = words.split 
-   maybe_anagram_two = letters_two.sort 
-   if maybe_anagram_one == maybe_anagram_two
-     words
-   end 
- end 
- end 
- 
+
    
   
   
