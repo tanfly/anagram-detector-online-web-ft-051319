8d07e2b8fddda5d46bb59319496095aa4d595c3a
Stella McEwen <mcewen.stella@gmail.com>
2019-05-24 15:31:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(maybe_anagrams_array)
+ maybe_anagrams_array.select do |words|
+   (@word.split("").sort == words.split("").sort)
+ end
+ end
+   
+  
+  
+end
