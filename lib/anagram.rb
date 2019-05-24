ec216e4b87a0c84fcbda4e6b2507a98769ec1174
Stella McEwen <mcewen.stella@gmail.com>
2019-05-24 15:30:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,9 @@ class Anagram
   
   def match(maybe_anagrams_array)
  maybe_anagrams_array.select do |words|
-   @word.split.sort == words.split.sort
+   @word.split.sort == words.split.sort 
+ end
+ end
    
   
   
